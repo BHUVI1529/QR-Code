c768e482b249fcacbf9d9dcaed9b5b5901140fab
Satyasree341 <satyasreeg2002@gmail.com>
2024-11-20 11:52:17 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('https://scanqr-jdez.onrender.com/', { email, password });
+            const response = await axios.post('https://scanqr-jdez.onrender.com/api/login', { email, password });
             const loginResponse = response.data;
 
             // Save token, user ID, and role to local storage
